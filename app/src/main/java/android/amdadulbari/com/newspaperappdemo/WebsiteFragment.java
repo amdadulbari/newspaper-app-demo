74f13e1a8fe193ff4c9ad61da475a9824db030d1
Amdadul Bari Imad <amdadulbari@gmail.com>
2017-04-02 00:30:37 +0600

    basic structure implemented

---
+++ app/src/main/java/android/amdadulbari/com/newspaperappdemo/WebsiteFragment.java
@@ -0,0 +1,57 @@
+package android.amdadulbari.com.newspaperappdemo;
+
+import android.app.Fragment;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+/**
+ * Created by voot on 4/1/17.
+ */
+
+public class WebsiteFragment extends Fragment {
+    private WebView webView;
+    private String urlOfWebsite = "";
+    private ProgressBar progressBar;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View mView = inflater.inflate(R.layout.fragment_website_view, container, false);
+        urlOfWebsite = getArguments().getString("url", "http://www.google.com");
+        webView = (WebView) mView.findViewById(R.id.webview);
+        progressBar = (ProgressBar) mView.findViewById(R.id.progressBar);
+        webView.setWebViewClient(new WebViewClient() {
+            public void onReceivedError(WebView view, int errorCode, String description, String failingUrl) {
+                Toast.makeText(getActivity(), "Oh no! " + description, Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onPageStarted(WebView view, String url, Bitmap favicon) {
+                super.onPageStarted(view, url, favicon);
+                progressBar.setVisibility(View.VISIBLE);
+
+            }
+
+
+
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                super.onPageFinished(view, url);
+                progressBar.setVisibility(View.GONE);
+            }
+        });
+        webView.loadUrl(urlOfWebsite);
+        webView.getSettings().setLoadsImagesAutomatically(true);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.setScrollBarStyle(View.SCROLLBARS_INSIDE_OVERLAY);
+        return mView;
+    }
+}
